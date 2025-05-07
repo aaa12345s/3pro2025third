0dab469d5ace7127841cbb3f700e41d02b9ee7b1
串田紗彩 <kushidasaya@a-2.local>
2025-05-07 14:12:21 +0900

    forth

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import './App.css'
 
 function App() {
   const pal = ["pink","white"];
-  const S = 5;
+  const S = 3;
   const [cols, setCols] = useState(Array(S*S).fill(0));  
 
   function clickHandler(i:number){
+    if(completed()){
+      return;
+    }
     const newcols = cols.slice();
     newcols[i]=1-newcols[i];
     if (i-S >= 0){
@@ -22,23 +25,29 @@ function App() {
     if (i%S+1 < S){
       newcols[i+1]=1-newcols[i+1];
     }
-    
     setCols(()=>newcols);
   }
 
+  function completed(){
+    for(let i=0;i<S*S;i++){
+      if(cols[i]==0){
+        return false;
+      }
+    }
+    return true;
+  }
 
   function Cell({i}:{i:number}){
+    const x=100*(i%S);
+    const y=100*Math.floor(i/S);
     return(
-      <>
+      <g onClick={()=>clickHandler(i)} transform={"translate("+x+","+y+")"}>
         <rect width={100} height={100}
-        x={100*(i%S)}  
-        y={100*Math.floor(i/S)}
         stroke="black" fill={pal[cols[i]]}
-        onClick={()=>clickHandler(i)}>      
+        >      
         </rect>
-        <text fill="red" x={100*(i%S)+50} 
-        y={100*Math.floor(i/S)+50}>{i}</text>
-      </>
+        <text fill="red" x={50} y={50}>{i}</text>
+      </g>
     )
   }
 
@@ -56,9 +65,19 @@ function App() {
     )
   }
 
+  function Finished(){
+    if(completed()){
+      return(<h1>完成です。おめでとうございます。</h1>)
+    }
+    else{
+      return(<h1>未完成です。頑張ってください。</h1>)
+    }
+  }
+
   return (
     <>
       <div>
+        <Finished></Finished>
         <h1>My first react project</h1>
         <svg width={800} height={800}>
           <Board></Board>
